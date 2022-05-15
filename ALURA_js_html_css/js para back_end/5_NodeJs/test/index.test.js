c8fdc8fc02163927ad6f9c747542d2d7a96e89a2
italoandradejr <italoandradejr@gmail.com>
2022-05-15 14:29:27 -0300

    alterações

--- ALURA_js_html_css/js para back_end/5_NodeJs/test/index.test.js
+++ ALURA_js_html_css/js para back_end/5_NodeJs/test/index.test.js
@@ -1,7 +1,23 @@
 //import { expect, test } from '@jest/globals'
 import pegaArquivo from '../1_index.js';
 
+const arrayResult = [
+    {
+        FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+    }
+]
 
-test("deve ser uma função", () => {
-    expect(typeof pegaArquivo).toBe('function');
+describe('pegaArquivo::', () => {
+    it('deve ser uma função', () => {
+        expect(typeof pegaArquivo).toBe('function');
+    })
+    it('deve retornar array com resultados', async () => {
+        const resultado = await pegaArquivo('./test/arquivos/texto1.md')
+        expect(resultado).toEqual(arrayResult)
+    })
+    it('deve retornar msg NÃO há links', async () => {
+        const resultado = await pegaArquivo('./test/arquivos/texto1_semLinks.md')
+        expect(resultado).toBe('não há links')
+    })
 })
+
