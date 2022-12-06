f260c8ffbeb288018c991a12060a1bf2d25f6f7c
Italo Andrade <italo.andrade@savoynet.com>
2022-12-06 17:37:35 -0300

    atualização diaria 06/12/2022

---
+++ z_arquivos_de_estudo/manipular_Array.js
@@ -0,0 +1,14 @@
+const empresas = [
+    { nome: 'Samsung', valorDeMercado: 50, CEO: 'Kim Hyun Suk', anoDeCriacao: 1938 },
+    { nome: 'Microsoft', valorDeMercado: 415, CEO: 'Satya Nadella', anoDeCriacao: 1975 },
+    { nome: 'Intel', valorDeMercado: 117, CEO: 'Brian Krzanich', anoDeCriacao: 1968 },
+    { nome: 'Facebook', valorDeMercado: 383, CEO: 'Mark Zuckerberg', anoDeCriacao: 2004 },
+    { nome: 'Spotify', valorDeMercado: 30, CEO: 'Daniel Ek', anoDeCriacao: 2006 },
+    { nome: 'Apple', valorDeMercado: 845, CEO: 'Tim Cook', anoDeCriacao: 1976 }
+];
+
+const anoDeCriacaoDaEmpresa = empresas.filter(empresaAno => (empresaAno.anoDeCriacao > 2000));
+
+console.log(anoDeCriacaoDaEmpresa)
+
+//https://www.alura.com.br/artigos/manipulacao-de-array-com-map-filter-e-reduce
