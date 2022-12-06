29e52b22c0709d4c35764be5588735093ea45144
italoandradejr <italoandradejr@gmail.com>
2022-12-05 21:13:52 -0300

    atualizações 05/12/22

--- ALURA_js_html_css/js para back_end_2/src/index.js
+++ ALURA_js_html_css/js para back_end_2/src/index.js
@@ -1,14 +1,12 @@
 import fs from 'fs'
 import chalk from 'chalk'
 
-
 function extraiLinks(texto) {
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
     const capturas = [...texto.matchAll(regex)]
     const resultados = capturas.map(captura => ({ [captura[1]]: [captura[2]] }))
-    return resultados
+    return resultados.length !== 0 ? resultados : 'Não a links no arquivo'
 }
-
 
 function trataErro(erro) {
     throw new Error(chalk.red(erro.code, "não há arquivo no diretório"))
@@ -18,15 +16,16 @@ async function pegaArquivo(caminhoDoArquivo) {
     try {
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
-        console.log(extraiLinks(texto))
+        return extraiLinks(texto)
     } catch (erro) {
         trataErro(erro)
     } finally {
-        console.log(chalk.yellow('operação concluída'))
+        //console.log(chalk.yellow('operação concluída'))
     }
 }
 
-pegaArquivo('./arquivos/texto.md')
+export default pegaArquivo
+
 
 
 
