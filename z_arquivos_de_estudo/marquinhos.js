67bf02f6ed985581e49dc9ff765f8f23c657176e
Italo Andrade <italoandradejr@gmail.com>
2023-01-13 17:13:48 -0300

    continuação curso css

--- z_arquivos_de_estudo/marquinhos.js
+++ z_arquivos_de_estudo/marquinhos.js
@@ -1,35 +1,12 @@
-/*
-const xNumeros = [1, 2, 3, 4]
+
+const xNumeros = [0, 1, 2, 3]
 const numero = Math.floor(Math.random() * xNumeros.length)
+const buscaDiv = document.getElementById(numero)
 
 
-var div = document.getElementsByTagName("div")
-
-if (div.style.display == "none") {
-    div.style.display = "block";
-} else {
-    div.style.display = "none";
-}
-*/
-
-var visibilidade = true; //Variável que vai manipular o botão Exibir/ocultar
-
-function exibir() {
-    document.getElementById("dvConteudo").style.display = "block";
+if (buscaDiv.style.display == "none") {
+    document.getElementById(numero).style.display = "block"
 }
 
-function ocultar() {
-    document.getElementById("dvConteudo").style.display = "none";
-}
-
-function ocultarExibir() { // Quando clicar no botão.
-
-    if (visibilidade) {//Se a variável visibilidade for igual a true, então...
-        document.getElementById("dvConteudo").style.display = "none";//Ocultamos a div
-        visibilidade = false;//alteramos o valor da variável para falso.
-    } else {//ou se a variável estiver com o valor false..
-        document.getElementById("dvConteudo").style.display = "block";//Exibimos a div..
-        visibilidade = true;//Alteramos o valor da variável para true.
-    }
-}
+console.log(buscaDiv)
 
