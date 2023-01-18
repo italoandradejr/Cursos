b2f51db7bc0cf9cd868045f494746a949891725a
Italo Andrade <italoandradejr@gmail.com>
2023-01-18 17:32:57 -0300

    grid

---
+++ ALURA_js_html_css/js para back_end/7_js-arrays/atualizando-lista.js
@@ -0,0 +1,5 @@
+const nomes = ["joão", "Ana", "Caio", "Lara", "Marjorie", "leo"]
+
+nomes.splice(1,2, "Rodrigo")
+
+console.log(nomes)
---
+++ ALURA_js_html_css/js para back_end/juntando-sala.js
@@ -0,0 +1,6 @@
+const salaJs = ['Evaldo', 'Camis', "mari"]
+const salaPython = ["Ju", "Leo", "Raquel"]
+
+const salasUnificadas = salaJs.concat(salaPython)
+
+console.log(salasUnificadas)
