2209dbe5d64572124c2ca60a0b651ed37e577534
italoandradejr <italoandradejr@gmail.com>
2023-08-16 22:27:06 -0300

    js para web

--- aluraplay-requisicoes/js/conectaApi.js
+++ aluraplay-requisicoes/js/conectaApi.js
@@ -1,13 +1,32 @@
-async function listaVideos () {
+async function listaVideos() {
     const conexao = await fetch("http://localhost:3000/videos")
     const conexaoConvertida = await conexao.json()
     //console.log(conexaoConvertida)
     return conexaoConvertida
 }
 
+async function criaVideo(titulo, descricao, url, imagem) {
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    })
+
+    const conexaoConvertida = await conexao.json()
+    return conexaoConvertida
+}
+
 
 export const conectaApi = {
-    listaVideos
+    listaVideos,
+    criaVideo
 }
 
 
