3d4a1aa1b34bbe57ef0a31c41ec61f66a30a357e
italoandradejr <italoandradejr@gmail.com>
2023-08-23 22:37:27 -0300

    apiJs

--- aluraplay-requisicoes/js/conectaApi.js
+++ aluraplay-requisicoes/js/conectaApi.js
@@ -19,6 +19,9 @@ async function criaVideo(titulo, descricao, url, imagem) {
         })
     })
 
+    if(!conexao.ok) {
+        throw new Error ("Não foi possível enviar o video")
+    }
     const conexaoConvertida = await conexao.json()
     return conexaoConvertida
 }
--- aluraplay-requisicoes/js/monstrarVideos.js
+++ aluraplay-requisicoes/js/monstrarVideos.js
@@ -18,9 +18,14 @@ export default function constroiCard(titulo, descricao, url, imagem) {
 }
 
 async function listaVideosDom() {
-    const listaApi = await conectaApi.listaVideos()
-    listaApi.forEach(elemento => lista.appendChild(
-        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+    try {
+        const listaApi = await conectaApi.listaVideos()
+        listaApi.forEach(elemento => lista.appendChild(
+            constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+    } catch {
+        lista.innerHTML = `<h2 class="mensagem__titulo">Não foi possivel carregar a lista de videos</h2>`
+    }
+
 }
 
 listaVideosDom()
