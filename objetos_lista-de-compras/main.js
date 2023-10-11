d4e011a9c3747ed1ad4fd9c5cb087c517cf58de2
italoandradejr <italoandradejr@gmail.com>
2023-10-10 22:00:44 -0300

    obs_js

---
+++ objetos_lista-de-compras/main.js
@@ -0,0 +1,45 @@
+let listaDeItens = []
+
+const form = document.getElementById("form-itens")
+const itensInput = document.getElementById("receber-item")
+const ulItens = document.getElementById("lista-de-itens")
+
+form.addEventListener("submit", function (evento) {
+    evento.preventDefault()
+    salvarItens()
+    mostrarItem()
+})
+
+function salvarItens() {
+    const comprasItem = itensInput.value
+    const checarDuplicado = listaDeItens.some((elemento) =>
+        elemento.valor.toUpperCase() === comprasItem.toUpperCase())
+
+    if (checarDuplicado) {
+        alert("o item já exite")
+    } else {
+        listaDeItens.push({
+            valor: comprasItem
+        })
+    }
+
+    console.log(listaDeItens)
+}
+
+function mostrarItem () {
+    ulItens.innerHTML = ""
+    listaDeItens.forEach((elemento, index) => {
+        ulItens.innerHTML += `
+        <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+            <input type="checkbox" class="is-clickable" />
+            <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+        </div>
+        <div>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+    </li>
+        `
+    })
+}
+
