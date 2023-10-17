ac902297e6b2537a907f5743838f1e7bfd738636
Italo Andrade <italoandradejr@gmail.com>
2023-10-17 15:52:54 -0300

    lista_OO

--- objetos_lista-de-compras/main.js
+++ objetos_lista-de-compras/main.js
@@ -3,11 +3,13 @@ let listaDeItens = []
 const form = document.getElementById("form-itens")
 const itensInput = document.getElementById("receber-item")
 const ulItens = document.getElementById("lista-de-itens")
+const ulItensComprados = document.getElementById("itens-comprados")
 
 form.addEventListener("submit", function (evento) {
     evento.preventDefault()
     salvarItens()
     mostrarItem()
+    itensInput.focus()
 })
 
 function salvarItens() {
@@ -19,17 +21,32 @@ function salvarItens() {
         alert("o item já exite")
     } else {
         listaDeItens.push({
-            valor: comprasItem
+            valor: comprasItem,
+            checar: false
         })
     }
-
-    console.log(listaDeItens)
+    itensInput.value = ""
 }
 
-function mostrarItem () {
+function mostrarItem() {
     ulItens.innerHTML = ""
+    ulItensComprados.innerHTML = ""
+
     listaDeItens.forEach((elemento, index) => {
-        ulItens.innerHTML += `
+        if (elemento.checar) {
+            ulItensComprados.innerHTML += `
+    <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+            <input type="checkbox" checked class="is-clickable" />  
+            <span class="itens-comprados is-size-5">${elemento.valor}</span>
+        </div>
+        <div>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+    </li>
+            `
+        } else {
+            ulItens.innerHTML += `
         <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
         <div>
             <input type="checkbox" class="is-clickable" />
@@ -40,6 +57,27 @@ function mostrarItem () {
         </div>
     </li>
         `
+        }
     })
+    const inputsCheck = document.querySelectorAll('input[type="checkbox"]')
+
+    inputsCheck.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+            listaDeItens[valorDoElemento].checar = evento.target.checked
+            mostrarItem()
+        })
+    })
+
+    const deletarObjetos = document.querySelectorAll(".deletar")
+    
+    deletarObjetos.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+            listaDeItens.splice(valorDoElemento, 1)
+            mostrarItem()
+        })
+    })
+
 }
 
