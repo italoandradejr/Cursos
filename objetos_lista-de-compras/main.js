020b04c091d23363e3a1ad4a6c902a195709b641
Italo Andrade <italoandradejr@gmail.com>
2023-10-23 17:36:14 -0300

    OOJS

--- objetos_lista-de-compras/main.js
+++ objetos_lista-de-compras/main.js
@@ -1,4 +1,5 @@
 let listaDeItens = []
+let itemAEditar
 
 const form = document.getElementById("form-itens")
 const itensInput = document.getElementById("receber-item")
@@ -50,9 +51,10 @@ function mostrarItem() {
         <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
         <div>
             <input type="checkbox" class="is-clickable" />
-            <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+            <input type="text" class="is-size-5" value="${elemento.valor}" ${index !== Number(itemAEditar) ? 'disabled' : ''}></input>
         </div>
         <div>
+        ${ index === Number(itemAEditar) ? '<button onClick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>' : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'}
             <i class="fa-solid fa-trash is-clickable deletar"></i>
         </div>
     </li>
@@ -70,7 +72,7 @@ function mostrarItem() {
     })
 
     const deletarObjetos = document.querySelectorAll(".deletar")
-    
+
     deletarObjetos.forEach(i => {
         i.addEventListener('click', (evento) => {
             const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
@@ -78,5 +80,20 @@ function mostrarItem() {
             mostrarItem()
         })
     })
+
+    const editarItens = document.querySelectorAll(".editar")
+
+    editarItens.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            itemAEditar = evento.target.parentElement.parentElement.getAttribute('data-value')
+            mostrarItem()
+        })
+    })
 }
 
+function salvarEdicao() {
+    const itemEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text]`)
+    listaDeItens[itemAEditar].valor = itemEditado.value
+    itemAEditar = -1
+    mostrarItem()
+}
