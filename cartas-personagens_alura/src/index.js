978fc42d9ff5584aabfeb91aa3f366606d94d7d5
italoandradejr <italoandradejr@gmail.com>
2023-11-27 23:41:27 -0300

    estudo js

---
+++ cartas-personagens_alura/src/index.js
@@ -0,0 +1,22 @@
+import { Personagem } from "./modules/personagem.js"
+
+
+const personagemPedrinho = new Personagem ()
+personagemPedrinho.nome = 'Pedrinho'
+personagemPedrinho.mana = 12
+personagemPedrinho.vida = 7
+personagemPedrinho.tipo = 'Mago'
+personagemPedrinho.level = 5
+
+
+const personagemJose = new Personagem()
+personagemJose.nome = 'Jose'
+personagemJose.tipo = 'Arqueiro'
+personagemJose.level = 3
+
+
+console.log(personagemPedrinho)
+console.log(`Insignia de ` + personagemPedrinho.nome + personagemPedrinho.obterInsignia())
+console.log("-------------------------------")
+console.log(personagemJose)
+console.log(`Insignia de ` + personagemJose.nome + personagemJose.obterInsignia())
