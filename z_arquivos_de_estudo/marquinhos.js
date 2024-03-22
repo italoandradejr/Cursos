2d5476b260316fbe7c82b85374d16250ffede4f5
Italo Andrade <italoandradejr@gmail.com>
2024-03-22 18:17:59 -0300

    2203

--- z_arquivos_de_estudo/marquinhos.js
+++ z_arquivos_de_estudo/marquinhos.js
@@ -1,4 +1,4 @@
-
+/*
 const xNumeros = [0, 1, 2, 3]
 const numero = Math.floor(Math.random() * xNumeros.length)
 const buscaDiv = document.getElementById(numero)
@@ -9,4 +9,18 @@ if (buscaDiv.style.display == "none") {
 }
 
 console.log(buscaDiv)
+*/
 
+let qtdNumeros = prompt('Digite a quantidade de números para o cálculo da média:');
+let soma = 0;
+let contador = qtdNumeros;
+
+while(contador > 0){
+    let numero = parseInt(prompt('Digite o numero:'));
+    soma += numero;
+    contador--
+}
+
+let media = soma / qtdNumeros;
+
+console.log(media);
